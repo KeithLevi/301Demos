eca840d6cee3a26e289f3dd42c46f6d95b0ffa09
KeithLevi <guilddrive@gmail.com>
2021-02-25 14:39:48 -0600

    savings exercise

---
+++ cs301_lecture_code/lesson4/continue.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+
+for (let i = 0; i < 10; i++) {
+    // if true, skip the remaining part of the body
+    //   if (i % 2 == 0) continue;
+    //   console.log(i); // 1, then 3, 5, 7, 9
+    if (i % 2 !== 0) {
+        console.log(i); // 1, then 3, 5, 7, 9
+    }
+}
--- cs301_lecture_code/lesson4/test_prime_using_break.js
+++ cs301_lecture_code/lesson4/test_prime_using_break.js
@@ -5,6 +5,16 @@ const prompt = require("prompt-sync")();
 let number = prompt("Please enter a number to test if it is pime: ");
 let isPrime = true;
 
+let i = 2;
+while (isPrime === true){
+  if (number % i == 0) {
+    isPrime = false;
+  }
+  i = i + 1;
+}
+
+
+
 for (let i = 2; i < number; i++) {
   if (number % i == 0) {
     isPrime = false;
