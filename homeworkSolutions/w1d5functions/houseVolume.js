bc80fef236da15322bee2824068f19b9ff1c4d99
Keith Levi <guilddrive@gmail.com>
2021-02-26 19:33:03 -0600

    Create houseVolume.js

---
+++ homeworkSolutions/w1d5functions/houseVolume.js
@@ -0,0 +1,37 @@
+"use strict";
+/* eslint-disable */
+/*
+3.	Write a program that calculates the total volume of a house, including the volume of the roof space. Your program must allow the user to 
+enter width, depth, height and sweep as shown in figure below. 
+houseVolume = livingVolume + roofVolume
+	livingVolume = width * height * depth
+	roofVolume = triangleArea*depth
+	triangleArea = √s(s−a)(s−b)(s−c)
+	s = (a+b+c)/2
+*/
+
+function houseVolume(width, height, depth, sweep){
+    const lv = livingVolume(width, height, depth);
+    const rv = roofVolume(sweep, width, depth);
+    return  lv + rv;  
+}
+
+function livingVolume(width, height, depth){
+    return width * height * depth;
+}
+
+function roofVolume(sweep, width, depth){
+    return triangleArea(sweep, sweep, width) * depth;
+}
+
+function triangleArea(sideA, sideB, sideC){
+    const s = (sideA + sideB + sideC)/2
+    const product = s * (s - sideA) * (s - sideB) * (s - sideC);
+    const result = Math.sqrt(product);
+    return result;
+}
+
+console.log("expect 2080", houseVolume(16,10,10,10));
+//roof:  8 * 6 = 48, * 10 = 480
+// living 10*10*16 = 1600
+//tot 2080
