815f09c9bf75f296602036034212410791c4cf5c
Keith Levi <guilddrive@gmail.com>
2021-03-03 09:21:53 -0600

    Update lcm.js

--- homeworkSolutions/w1d5functions/lcm.js
+++ homeworkSolutions/w1d5functions/lcm.js
@@ -26,3 +26,26 @@ function leastCommonMultiple(num1, num2) {
  process:
   LCM = product / GCD  
  */
+
+ /* 7.  Write a function compute, that takes three parameters. First parameter is a call back function that does the actual operation, second and third are the operands.
+For e.g., compute (add, 2,3) should return 5
+- Write compute function as a function expression.
+- Write add function as a function expression and call compute passing add as a callback.
+o Refactor to pass add logic as an anonymous function. (Make a copy first)
+o Refactor to pass add logic as an arrow function. (Make a copy first)
+
+*/
+
+
+
+const compute = function(computeFunc, num1, num2){
+    return computeFunc(num1, num2);
+};
+
+const add = function(number1, number2){
+    return number1 + number2;
+};
+
+console.log("compute(2,3) should return 5 : ", compute(add, 2, 3));
+
+
