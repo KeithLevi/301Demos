8e35a8526ba8728575cb393ed1c2281328c031a4
Keith Levi <guilddrive@gmail.com>
2021-03-02 21:29:09 -0600

    changes to folder names

---
+++ cs301_lecture_code/lesson7CodeQuality/person.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+const sam1 = {name: "Sam", age: 10};
+const sam2 = {name: "Sam", age: 10};
+const john = {name: "John", age: 10};
+
+/**
+ * 
+ * @param {Object} pers1 is a person object
+ * @param {Object} pers2 is a person object
+ * @returns {boolean} true if both properties match
+ */
+function isPersonEqual(pers1, pers2){
+    /* IMPLEMENTATION NEEDED */
+    console.log("this needs code!!!");
+}
+
+console.log(isPersonEqual(sam1, sam2));
+console.log(isPersonEqual(sam1, john));
