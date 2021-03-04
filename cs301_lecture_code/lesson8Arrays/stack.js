4456325ee6c98a78ae09d5a2fa1375c4cc627730
Keith Levi <guilddrive@gmail.com>
2021-03-04 13:45:13 -0600

    Update stack.js

--- cs301_lecture_code/lesson8Arrays/stack.js
+++ cs301_lecture_code/lesson8Arrays/stack.js
@@ -12,23 +12,51 @@
        -- else not balanced
 
 when empty original stack the closeStack must also be empty, else unbalanced
-
+*/
 const stack = ["(", "(", ")", ")"];
 
-let balanced = true;
-while (stack.length > 0) {
-    const top1 = stack.pop();
-    if (top1 === ")" || top1 === "}") {
-        const top2 = stack.pop();
-        if (top1 !== top2) {
-            //if does not match then should save it on a second stack -- closeStack
-
- 
+/**
+ * 
+ * @param {Array} stack is array of ( or )
+ * @returns {boolean} true if balanced
+ */
+function checkBalance(stack) {
+     let closeStack = [];
+    while (stack.length > 1) {
+        const top = stack.pop();
+        if (top === ")") {
+            //-- if next is a matching open bracket then (discard both and ) continue process
+            if (stack[stack.length-1] === "(") {
+                stack.pop();
+                continue;
+            } else {  //if next is another closing bracket then push top onto closeStack
+                closeStack.push(top);
+            }
+        } else { //top is "(", must match the top of closeStack
+            if (closeStack.length  === 0 || top !== closeStack.pop()) {
+                return false;
+            } //else top of stack and of closeStack match and have both been popped, so continue with loop.  are at end of loop here
         }
-    } else 
+    }
+    //now stack is empty or 1 element
+    if (stack.length === 1) { // check to see if it matches closeStack last element, else false}
+    const len = closeStack.length;
+    const closeTop =closeStack.pop();
+    const stackTop = stack.pop();
+        if ( len=== 1 && closeTop === ")" && stackTop === "(") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    if (stack.length === 0 && closeStack.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
+console.log("expect true: " , checkBalance(stack));
 
-//            console.log("unbalanced!!");
-// balanced = false;
-// break;
+
